3b20e1ba49b39a7e4bfca71970cb738accb0c763
fredie <lexfredeick3@gmail.com>
2020-01-09 10:12:58 +0100

    signup api id correction

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,18 +1,31 @@
 import data from '../data/users';
 
-const JWT = require('jsonwebtoken');
-const bycrypt = require('bcryptjs')
-require ('dotenv').config();
+// const JWT = require('jsonwebtoken');
+// const bycrypt = require('bcryptjs')
+// require ('dotenv').config();
 
-const { newID } = '../helpers/helper';
+
+const newID = () => {
+  let id;
+  if (data.length > 0) {
+    id = data[data.length - 1].id + 1;
+  } else {
+    id = 1;
+  }
+  return id;
+};
 
 const authController = {
   signup: (req, res) => {
-    const { name, email, password } = req.body;
-    const id = newID;
-    const userExists = data.find((info) => info.id === id);
+    const newUser = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      id: newID(),
+    };
+    const userExists = data.find((info) => info.id === newUser.id);
     if (!userExists) {
-      data.push(req.body);
+      data.push(newUser);
       res.status(201).json({ message: 'you are now signed up' });
     } else {
       res.status(400).json({ error: 'user already exists' });
