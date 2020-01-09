0b4d79cb4d773e4da0aea00a3e6f67d09c8c6f38
fredie <lexfredeick3@gmail.com>
2020-01-09 20:46:25 +0100

    new user-signup third update

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,17 +15,16 @@ const authController = {
   signup: (req, res) => {
     const userExists = data.find((info) => info.email === req.body.email);
     if (userExists) {
-      res.status(400).json({ error: 'user already exists' });
-    } else {
-      const newUser = {
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        id: newID(),
-      };
-      data.push(newUser);
-      res.status(201).json(newUser);
+      return res.status(400).json({ error: 'user already exists' });
     }
+    const newUser = {
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      id: newID(),
+    };
+    data.push(newUser);
+    res.status(201).json(newUser);
   },
 };
 export default authController;
