505ddc38b0726fbf99396949fcf9c04470815255
fredie7 <52379604+fredie7@users.noreply.github.com>
2020-01-15 22:31:01 +0100

    validation now created for signup and signin routes (#5)
    * validation now created for signup and signin routes
    
    * validator corrections fixed
    
    * status code updated and test folder deleted

--- routers/auth.js
+++ routers/auth.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import authController from '../controllers/auth';
+import validator from '../validator';
 
 const router = express.Router();
 
+const { signinValidator, signupValidator } = validator;
 const { signup, signin } = authController;
 
-router.post('/signup', signup);
-router.post('/signin', signin);
+router.post('/signup', signupValidator, signup);
+router.post('/signin', signinValidator, signin);
 
 module.exports = router;
