60011fc1170df4d6e8cdd7ff49334bba3cd72a4a
fredie7 <52379604+fredie7@users.noreply.github.com>
2020-02-24 11:55:18 +0100

    Ft cancel parcel order (#14)
    * cancel order
    
    * cancel order pseudocode
    
    * cancel parcel order fix
    
    * cancel delivery order fix
    
    * cancel order fx
    
    * cancel parcel fix
    
    * cancel order fix
    
    * cancel parcel order
    
    * cancel parcel fixed
    
    * fix conflicts

--- src/controllers/parcel.js
+++ src/controllers/parcel.js
@@ -36,6 +36,7 @@ const parcelController = {
       description: req.body.description || foundParcel.description,
       weight: req.body.weight || foundParcel.weight,
       updatedAt: new Date(),
+      status: 'pending',
     };
     const parcelIndex = parcels.indexOf(foundParcel);
     parcels.splice(parcelIndex, 1, updatedParcel);
@@ -43,11 +44,33 @@ const parcelController = {
   }),
 
   getOneParcel: ((req, res) => {
-    const parcel = parcels.find((parcel) => parcel.id === req.params.parcelId);
-    if (!parcel) {
+    const foundParcel = parcels.find((parcel) => parcel.id === req.params.parcelId);
+    if (!foundParcel) {
       return res.status(404).json({ error: 'parcel not found' });
     }
-    return res.status(200).json(parcel);
+    return res.status(200).json(foundParcel);
+  }),
+
+  cancelParcelOrder: ((req, res) => {
+    const newParcel = {
+      id: uuidV4(),
+      createdBy: req.decoded.id,
+      pickupLocation: req.body.pickupLocation,
+      deliveryLocation: req.body.deliveryLocation,
+      presentLocation: req.body.presentLocation,
+      receiverPhone: req.body.receiverPhone,
+      receiverEmail: req.body.receiverEmail,
+      description: req.body.description,
+      weight: req.body.weight,
+    };
+    parcels.push(newParcel);
+
+    const foundParcel = parcels.find((parcel) => parcel.id === newParcel.id);
+
+    if (!foundParcel) {
+      return res.status(404).json({ error: 'parcel not found' });
+    }
+    return res.json({ message: 'parcel order cancelled' });
   }),
 };
 
--- src/routers/parcel.js
+++ src/routers/parcel.js
@@ -5,14 +5,18 @@ import parcelValidation from '../middlewares/validations/parcelValidation';
 
 const { createParcelValidation, editParcelValidation } = parcelValidation;
 const { verifyToken } = middlewares;
-const { createParcel, editParcel, getOneParcel } = parcelController;
-
+const {
+  createParcel,
+  editParcel,
+  getOneParcel,
+  cancelParcelOrder,
+} = parcelController;
 
 const router = express.Router();
 
 router.post('/parcels', verifyToken, createParcelValidation, createParcel);
 router.put('/parcels/:parcelId', verifyToken, editParcelValidation, editParcel);
 router.get('/parcels/:parcelId', verifyToken, getOneParcel);
-
+router.put('/parcels/:parcelId/cancel', cancelParcelOrder);
 
 module.exports = router;
