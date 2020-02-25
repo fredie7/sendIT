b49a6fa7347c2bb72f04a3aa3fd34873b6e5b3ca
fredie <lexfredeick3@gmail.com>
2020-02-25 14:07:26 +0100

    get all parcels

--- src/tests/parcels.js
+++ src/tests/parcels.js
@@ -364,6 +364,16 @@ describe('PUT /api/v1/parcels/:parcelId', () => {
         });
     });
   });
+
+  it('should return all parcels', (done) => {
+    chai.request(app)
+      .get('/api/v1/parcels')
+      .set('authorization', user.token)
+      .end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+  });
 });
 
 
