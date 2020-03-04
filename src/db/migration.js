ba10e3d789e8ed7409e8017b655d62e20a2a4601
John Samuel <jsamchineme@gmail.com>
2020-03-04 12:02:40 +0000

    fix migrations

--- src/db/migration.js
+++ src/db/migration.js
@@ -5,27 +5,27 @@ import logger from '../services/logger';
 // import seedDatabase from '../db/seed';
 
 const queryText = `
-CREATE EXTENSION IF NOT EXISTS 'uuid-ossp';
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS parcels;
 
-CREATE TABLE IF NOT EXIST users (
+CREATE TABLE IF NOT EXISTS users (
   "id" UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
-  "name VARCHAR(100) NOT NULL",
+  "name" VARCHAR(100) NOT NULL,
   "email" VARCHAR(100) UNIQUE NOT NULL,
-  "password VARCHAR(100) NOT NULL",
-)
+  "password" VARCHAR(100) NOT NULL
+);
 
-CREATE TABLE IF NOT EXIST Parcels (
+CREATE TABLE IF NOT EXISTS parcels (
   "id" UUID PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
-  "pickupLocation VARCHAR(100) NOT NULL",
+  "pickupLocation" VARCHAR(100) NOT NULL,
   "deliveryLocation" VARCHAR(100) NOT NULL,
   "description" VARCHAR(100) NOT NULL,
-  "receiverPhone"  "deliveryLocation" INT UNIQUE NOT NULL,
-  "receiverEmail"  "deliveryLocation" VARCHAR(100) UNIQUE NOT NULL,
+  "receiverPhone" VARCHAR(20) NOT NULL,
+  "receiverEmail" VARCHAR(100) UNIQUE NOT NULL,
   "presentLocation" VARCHAR(100) NOT NULL,
-  "weight"  "deliveryLocation" INT UNIQUE NOT NULL,
-)
+  "weight" VARCHAR(11) NOT NULL
+);
 `;
 
 db.query(queryText)
@@ -34,6 +34,6 @@ db.query(queryText)
     process.exit(0);
   })
   .catch((error) => {
-      logger.info(error)
-      process.exit(1);
-    });
+    logger.info(error);
+    process.exit(1);
+  });
