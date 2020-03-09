1ddb88af3bfea47ef3b6e22a3a9f5d3021716e3a
fredie <lexfredeick3@gmail.com>
2020-03-09 08:14:32 +0100

    seed comments fix

--- src/db/index.js
+++ src/db/index.js
@@ -4,12 +4,21 @@ import logger from '../services/logger';
 
 dotenv.config();
 
-// this should be made dynamic based on the environment
-// development, test, production
-const connectionString = process.env.DATABASE_DEV_URL;
+let connectionString;
+if (process.env.NODE_ENV === 'development') {
+  connectionString = process.env.DATABASE_DEV_URL;
+} else if (process.env.NODE_ENV === 'test') {
+  connectionString = process.env.DATABASE_TEST_URL;
+} else {
+  connectionString = process.env.DATABASE_PRODUCTION_URL;
+}
+
+// const connectionString = process.env.DATABASE_DEV_URL;
 logger.info(`NODE_environment: ${process.env.NODE_ENV}`)
 
-const db = new Pool({ connectionString });
+const db = new Pool({ 
+  connectionString,
+});
 
 // this was the missing piece.
 // the database connection was not being established
